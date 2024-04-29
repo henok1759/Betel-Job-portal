d3d6f79ac5263a824dbfc6960025f1e9db776063
Henok Metaferia <139316869+henok1759@users.noreply.github.com>
2024-04-30 00:35:49 +0600

    Add files via upload
    Job Seeker

---
+++ JobSeeker/footer.php
@@ -0,0 +1,90 @@
+<link rel="stylesheet" type="text/css" href="../css/footer.css" media="screen">
+<footer class="footer-wrapper">
+			
+            <div class="main-footer">
+            
+                <div class="container">
+                
+                    <div class="row">
+                    
+                        <div class="col-sm-12 col-md-9">
+                        
+                            <div class="row">
+                            
+                                <div class="col-sm-6 col-md-4">
+                                
+                                <div class="footer-about-us">
+                                        <h5 class="footer-title">About Bethel computer center</h5>
+                                        <p>Bethel computer center is a job portal, online job management system developed by Biruk Yehualashet and Henok Metaferia for those project in 2023.</p>
+                                    
+                                    </div>
+
+                                </div>
+                                
+                                <div class="col-sm-6 col-md-5 mt-30-xs">
+                                    <h5 class="footer-title">Quick Links</h5>
+                                    <ul class="footer-menu clearfix">
+                                        <li><a href="./">Home</a></li>
+                                        <li><a href="Profile.php">Profile</a></li>
+                                        <li><a href="Education.php">Education</a></li>
+                                        <li><a href="SearchJob.php">Search Job</a></li>
+                                        <li><a href="Walkin.php">Walkin Interview</a></li>
+                                        <li><a href="Feedback.php">Feedback</a></li>
+                                        <li><a href="#">Go to top</a></li>
+
+                                    </ul>
+                                
+                                </div>
+
+                            </div>
+
+                        </div>
+                        
+                        <div class="col-sm-12 col-md-3 mt-30-sm">
+                        
+                            <h5 class="footer-title">Bethel computer center Contact</h5>
+                            
+                            <p>Address : Debrebrhan ,In front of Hospital</p>
+                            <p>Email : <a href="Bethel2@gmail.com">Bethel2@gmail.com</a></p>
+                            <p>Telegram : <a href="@bethel">@bethel</a></p>
+                            <p>Phone : <a href="+251900000000">+251900000000</a></p>
+                            
+
+                        </div>
+
+                        
+                    </div>
+                    
+                </div>
+                
+            </div>
+            
+            <div class="bottom-footer">
+            
+                <div class="container">
+                
+                    <div class="row">
+                    
+                        <div class="col-sm-4 col-md-4">
+                
+                            <p class="copy-right">&#169; Copyright <?php echo date('Y'); ?> Bethel computer and software training company</p>
+                            
+                        </div>
+                        
+                        <div class="col-sm-4 col-md-4">
+                        
+                            <ul class="bottom-footer-menu">
+                                <li><a >Developed by Biruk Yehualashet and Henok Metaferia</a></li>
+                            </ul>
+                        
+                        </div>
+                        
+                        
+                    
+                    </div>
+
+                </div>
+                
+            </div>
+        
+        </footer>
---
+++ JobSeeker/right.php
@@ -0,0 +1,59 @@
+        <!-- Right column -->
+<?php
+
+?>
+<style type="text/css">
+<!--
+.style1 {font-size: 18px}
+-->
+</style>
+
+
+<div id="col" class="noprint">
+            <div id="col-in">
+
+                <!-- About Me -->
+                <h3>Welcome</h3>
+                <p>Authority of an Job Seeker are:<br />
+                    <ul>
+                      <li>Manage Profile</li>
+                      <li>Manage Education</li>
+                      <li>Search Job</li>
+                      <li>Walkin Interview</li>
+                      <li>Give Feedback</li>
+                    </ul>
+                    </p>
+
+                <div id="about-me">
+                    <p class="style1">
+                      <?php 
+					
+					?>
+                    </p>
+              </div> 
+                <!-- /about-me -->
+
+                <hr class="noscreen" />
+
+                <!-- Category -->
+                <h3 >&nbsp;</h3>
+
+              <br/>
+
+      <hr class="noscreen" />
+
+                <!-- Archive -->
+                <h3>&nbsp;</h3>
+
+                <br/>
+
+            <hr class="noscreen" />
+
+                <!-- Links -->
+                <h3>&nbsp;</h3>
+<br/>
+
+<hr class="noscreen" />
+          </div> <!-- /col-in -->
+</div> 
+<!-- /col -->
