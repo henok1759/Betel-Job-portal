12a733c412ff5ea26686bd2ba1cfa9d2e925ff30
Henok Metaferia <139316869+henok1759@users.noreply.github.com>
2024-04-30 00:42:37 +0600

    Add files via upload
    footer

---
+++ footer.php
@@ -0,0 +1,90 @@
+<link rel="stylesheet" type="text/css" href="css/footer.css" media="screen">
+<footer class="footer-wrapper">
+			
+            <div class="main-footer">
+            
+                <div class="container">
+                
+                    <div class="row">
+                    
+                        <div class="col-sm-12 col-md-9">
+                        
+                            <div class="row">
+                            
+                                <div class="col-sm-6 col-md-4">
+                                
+                                <div class="footer-about-us">
+                                        <h5 class="footer-title">About Bethel computer center</h5>
+                                        <p>Bethel computer center is a job portal, online job management system developed by Biruk Yehualashet and Henok Metaferia for those project in 2023.</p>
+                                    
+                                    </div>
+
+                                </div>
+                                
+                                <div class="col-sm-6 col-md-5 mt-30-xs">
+                                    <h5 class="footer-title">Quick Links</h5>
+                                    <ul class="footer-menu clearfix">
+                                        <li><a href="./">Home</a></li>
+                                        <li><a href="AboutUs.php">About Us</a></li>
+                                        <li><a href="Employer.php">Employer</a></li>
+                                        <li><a href="JobSeeker.php">Job Seeker</a></li>
+                                        <li><a href="News.php">Latest News</a></li>
+                                        <li><a href="ContactUs.php">Contact Us</a></li>
+                                        <li><a href="#">Go to top</a></li>
+
+                                    </ul>
+                                
+                                </div>
+
+                            </div>
+
+                        </div>
+                        
+                        <div class="col-sm-12 col-md-3 mt-30-sm">
+                        
+                            <h5 class="footer-title">Bethel computer center Contact</h5>
+                            
+                            <p>Address : Debrebrhan ,In front of Hospital</p>
+                            <p>Email : <a href="Bethel2@gmail.com">Bethel2@gmail.com</a></p>
+                            <p>Telegram : <a href="@bethel">@bethel</a></p>
+                            <p>Phone : <a href="+251900000000">+251900000000</a></p>
+                            
+
+                        </div>
+
+                        
+                    </div>
+                    
+                </div>
+                
+            </div>
+            
+            <div class="bottom-footer">
+            
+                <div class="container">
+                
+                    <div class="row">
+                    
+                        <div class="col-sm-4 col-md-4">
+                
+                            <p class="copy-right">&#169; Copyright <?php echo date('Y'); ?> Bethel computer and software training company</p>
+                            
+                        </div>
+                        
+                        <div class="col-sm-4 col-md-4">
+                        
+                            <ul class="bottom-footer-menu">
+                                <li><a >Developed by Biruk Yehualashet and Henok Metaferia</a></li>
+                            </ul>
+                        
+                        </div>
+                        
+                    
+                    
+                    </div>
+
+                </div>
+                
+            </div>
+        
+        </footer>
