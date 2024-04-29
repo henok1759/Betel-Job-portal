bd13a2b39d00fc39d1d0441ec44f3a85de6f4257
Henok Metaferia <139316869+henok1759@users.noreply.github.com>
2024-04-30 00:28:58 +0600

    Add files via upload
    connection

---
+++ Connections/job.php
@@ -0,0 +1,16 @@
+<?php
+# FileName="Connection_php_mysql.htm"
+# Type="MYSQL"
+# HTTP="true"
+$hostname_job = "localhost";
+$database_job="sourcecodester_jobportaldb";
+$username_job = "root";
+$password_job = "";
+$job;
+$database_job = mysqli_connect($hostname_job, $username_job, $password_job,"sourcecodester_jobportaldb") or trigger_error(mysqli_error(),E_USER_ERROR);*/
+
+
+
+
+
+?>
