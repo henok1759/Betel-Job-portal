edb89e55b8ce53ba88d7a17b6c6952290430e528
Henok Metaferia <139316869+henok1759@users.noreply.github.com>
2024-04-30 00:27:02 +0600

    Add files via upload
    admin

---
+++ Admin/index.php
@@ -0,0 +1,109 @@
+<?php
+session_start();
+if(isset($_SESSION['$UserName'])){
+
+} 
+else{
+		header('location:../index.php');
+}
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="cs" lang="cs">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta http-equiv="content-language" content="cs" />
+    <meta name="robots" content="all,follow" />
+
+    
+    <title>JOB PORTAL BY JITESH</title>
+    <meta name="description" content="..." />
+    <meta name="keywords" content="..." />
+    
+    <link rel="index" href="./" title="Home" />
+    <link rel="stylesheet" media="screen,projection" type="text/css" href="./css/hole.css" />
+    <link rel="stylesheet" media="screen,projection" type="text/css" href="../bootstrap/css/bootstrap.min.css" />
+    <link rel="stylesheet" media="print" type="text/css" href="./css/print.css" />
+    <link rel="stylesheet" media="aural" type="text/css" href="./css/aural.css" />
+    <style type="text/css">
+<!--
+.style1 {
+	color: #000066;
+	font-weight: bold;
+}
+-->
+    </style>
+</head>
+
+<body id="www-url-cz">
+<!-- Main -->
+<div id="main" class="box">
+<?php 
+
+include "Header.php"
+?>
+<?php 
+include "menu.php"
+?>   
+<!-- Page (2 columns) -->
+    <div id="page" class="box">
+    <div id="page-in" class="box">
+
+        <div id="strip" class="box noprint">
+
+            <!-- RSS feeds -->
+            <hr class="noscreen" />
+
+            <!-- Breadcrumbs -->
+            <p id="breadcrumbs">You are here: <a href="index.php">Home</a></p>
+          <hr class="noscreen" />
+            
+        </div> <!-- /strip -->
+
+        <!-- Content -->
+        <div id="content">
+
+           
+            <!-- /article -->
+
+            <hr class="noscreen" />
+
+           
+            <!-- /article -->
+
+            <hr class="noscreen" />
+            
+            <!-- Article -->
+           
+            <!-- /article -->
+
+            <hr class="noscreen" />
+
+            <!-- Article -->
+            <div class="article">
+                <marquee class="marq">Welcome To Adminstrator page</marquee>
+               
+
+                <p>&nbsp;</p>
+
+              <p class="btn-more box noprint">&nbsp;</p>
+          </div> <!-- /article -->
+
+            <hr class="noscreen" />
+            
+        </div> <!-- /content -->
+
+<?php
+include "right.php"
+?>
+
+    </div> <!-- /page-in -->
+    </div> <!-- /page -->
+
+ 
+<?php
+include "footer.php"
+?>
+</div> <!-- /main -->
+
+</body>
+</html>
---
+++ Admin/right.php
@@ -0,0 +1,45 @@
+        <!-- Right column -->
+
+
+<div id="col" class="noprint">
+            <div id="col-in">
+
+                <!-- About Me -->
+                <h3>Welcome</h3>
+
+                <div id="about-me">
+                    <p>Authority of an Administrator are:<br />
+                    <ul>
+                      <li>Manage system</li>
+                      <li>Manage Employer</li>
+                      <li>Post News</li>
+                      <li>See Feedback</li>
+                      <li>Add other Adminstrator</li>
+                    </ul>
+                    </p>
+              </div> <!-- /about-me -->
+
+                <hr class="noscreen" />
+
+                <!-- Category -->
+                <h3 >&nbsp;</h3>
+
+              <br/>
+
+      <hr class="noscreen" />
+
+                <!-- Archive -->
+                <h3>&nbsp;</h3>
+
+                <br/>
+
+            <hr class="noscreen" />
+
+                <!-- Links -->
+                <h3>&nbsp;</h3>
+<br/>
+
+<hr class="noscreen" />
+          </div> <!-- /col-in -->
+</div> 
+<!-- /col -->
