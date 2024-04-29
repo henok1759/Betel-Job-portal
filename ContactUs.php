236e9c8d786cf1ca1142d88edc30042b18a50f0a
Henok Metaferia <139316869+henok1759@users.noreply.github.com>
2024-04-30 00:39:51 +0600

    Add files via upload
    Contact Us

---
+++ ContactUs.php
@@ -0,0 +1,130 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="cs" lang="cs">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta http-equiv="content-language" content="cs" />
+    <meta name="robots" content="all,follow" />
+
+    
+    
+    <title>bethel computer</title>
+    <meta name="description" content="..." />
+    <meta name="keywords" content="..." />
+    
+    <link rel="index" href="./" title="Home" />
+    <link rel="stylesheet" media="screen,projection" type="text/css" href="css/hole.css" />
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" media="screen">
+    <link rel="stylesheet" media="print" type="text/css" href="./css/print.css" />
+    <link rel="stylesheet" media="aural" type="text/css" href="./css/aural.css" />
+    <style type="text/css">
+<!--
+.style1 {
+	color: #000066;
+	font-weight: bold;
+}
+-->
+    </style>
+    <script src="SpryAssets/SpryValidationTextarea.js" type="text/javascript"></script>
+    <link href="SpryAssets/SpryValidationTextarea.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body id="www-url-cz">
+<!-- Main -->
+<div id="main" class="box">
+<?php 
+include "Header.php"
+?>
+<?php 
+include "menu.php"
+?>   
+<!-- Page (2 columns) -->
+    <div id="page" class="box">
+    <div id="page-in" class="box">
+
+        <div id="strip" class="box noprint">
+
+            <!-- RSS feeds -->
+            <hr class="noscreen" />
+
+            <!-- Breadcrumbs -->
+            <p id="breadcrumbs">&nbsp;</p>
+          <hr class="noscreen" />
+            
+        </div> <!-- /strip -->
+
+        <!-- Content -->
+        <div id="content">
+
+           
+            <!-- /article -->
+
+            <hr class="noscreen" />
+
+           
+            <!-- /article -->
+
+            <hr class="noscreen" />
+            
+            <!-- Article -->
+           
+            <!-- /article -->
+
+            <hr class="noscreen" />
+
+            <!-- Article -->
+            <div class="article">
+                <h2><span><a href="#">Contact Us</a></span></h2>
+               
+
+                <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                  <tr>
+                    <td width="6%"><img src="design/ico_archive2.gif" alt="" width="9" height="11" /></td>
+                    <td width="94%"><strong>bethel.com<br/>
+                    Debrebrhan, Ethiopia<br/>
+                    P.O.BOX:0000</strong></td>
+                  </tr>
+                  <tr>
+                    <td><img src="design/ico_archive2.gif" alt="" width="9" height="11" /></td>
+					<td><strong>Office No:000000000<b>
+					Mobile: 000000000
+					</strong></td>
+					
+                  </tr>
+                  <tr>
+                    <td><img src="design/ico_archive2.gif" alt="" width="9" height="11" /></td>
+                    <td><strong>Email:bethel.com</strong></td>
+                  </tr>
+                  <tr>
+                    <td><img src="design/ico_archive2.gif" alt="" width="9" height="11" /></td>
+                    <td><strong>Website:bethel.com</strong></td>
+                  </tr>
+			
+                </table>
+				<p><img src="#" alt="" width="500" height="300" /></p>
+              
+              <p>&nbsp;</p>
+
+              <p class="btn-more box noprint">&nbsp;</p>
+          </div> <!-- /article -->
+
+            <hr class="noscreen" />
+            
+        </div> <!-- /content -->
+
+<?php
+include "right.php"
+?>
+
+    </div> <!-- /page-in -->
+    </div> <!-- /page -->
+
+ 
+<?php
+include "footer.php"
+?>
+</div> <!-- /main -->
+
+
+</body>
+</html>
